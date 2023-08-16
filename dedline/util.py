aa2653fb46d1207f8af4d1f70fa62aae0730844e
kigawa <kigawa.8390@gmail.com>
2023-08-16 18:14:24 +0900

    add: create result

--- dedline/util.py
+++ dedline/util.py
@@ -1,2 +1,9 @@
+from flask import jsonify, Response
 
 
+def create_result(data: dict, error: False, message: str = "") -> Response:
+    return jsonify({
+        "error": error,
+        "message": message,
+        "result": data
+    })
